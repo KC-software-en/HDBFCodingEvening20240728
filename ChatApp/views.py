80826da84d993e074b54eb8f7872f5dac6575f4f
KC_software_en <kisha.cairncross@gmail.com>
2024-07-18 19:44:26 +0200

    added index to ChatApp/views.py

--- ChatApp/views.py
+++ ChatApp/views.py
@@ -1,3 +1,11 @@
+# import render
 from django.shortcuts import render
+# import HttpResponse to test that the view shows after runserver
+from django.http import HttpResponse
 
 # Create your views here.
+# create a view for the homepage of the app - index. load html into the function
+def index(request):
+    # runserver to check if HttpResponse shows in webpage then comment out HttpResponse
+    # return HttpResponse("<h2>Hello World</h2>")
+    return render(request, "index.html")
