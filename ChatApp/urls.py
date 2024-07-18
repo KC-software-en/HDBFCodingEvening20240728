45200f5e5100dce0b35d5195e39471bc1480c407
KC_software_en <kisha.cairncross@gmail.com>
2024-07-19 00:21:08 +0200

    added a name for the index view in ChatApp/urls.py

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 # set a path to the index function in views.py
+# add a name for the index view
 urlpatterns = [
-path('', views.index),
+path('', views.index, name="index"),
 ]
