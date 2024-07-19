77444470571328b75d588125b44a52cf9a9d5328
KC_software_en <kisha.cairncross@gmail.com>
2024-07-19 23:00:44 +0200

    edited chat_room path in ChatApp/urls.py

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 # set a path to the index function in views.py
 # add a name for the index view
+# set a path to the chat_room function in views.py & name it
+# - NOTE: include ChatApp to explicitly match URLs that start with /ChatApp/ followed by a dynamic room name, 
+# - matching the pathname the JavaScript in index.html was set to redirect 
 urlpatterns = [
-path('', views.index, name="index"),
-]
+    path("", views.index, name="index"),
+    path("ChatApp/<str:room_name>/", views.chat_room, name="chat_room"),
+    ]
