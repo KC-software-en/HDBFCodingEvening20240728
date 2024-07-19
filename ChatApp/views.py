ba6d9038b96e3218e1b49f509f901496e91b223b
KC_software_en <kisha.cairncross@gmail.com>
2024-07-19 21:17:56 +0200

    Added docstr to index view in ChatApp/views.py

--- ChatApp/views.py
+++ ChatApp/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-# create a view for the homepage of the app - index. load html into the function
+# create a view for the homepage of the app - load index.html into the function
 def index(request):
+    """A view for the homepage of the ChatApp - index. load html into the function
+
+    :param request: The HTTP request object containing information about the client's request.
+    :type request: HttpRequest
+    :return: Return the index template
+    :rtype: HttpResponse
+    """
     # runserver to check if HttpResponse shows in webpage then comment out HttpResponse
     # return HttpResponse("<h2>Hello World</h2>")
     return render(request, "index.html")
