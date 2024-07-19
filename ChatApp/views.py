6ce9451af0207db9272e67912efc7012780b1b40
KC_software_en <kisha.cairncross@gmail.com>
2024-07-19 22:59:04 +0200

    added chat_room to ChatApp/views.py

--- ChatApp/views.py
+++ ChatApp/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-# create a view for the homepage of the app - load index.html into the function
+# create a view for the homepage of the app 
+# - load index.html into the function
 def index(request):
-    """A view for the homepage of the ChatApp - index. load html into the function
+    """A view for the homepage of the ChatApp.
 
     :param request: The HTTP request object containing information about the client's request.
     :type request: HttpRequest
@@ -15,4 +16,24 @@ def index(request):
     """
     # runserver to check if HttpResponse shows in webpage then comment out HttpResponse
     # return HttpResponse("<h2>Hello World</h2>")
+    # render the index template
     return render(request, "index.html")
+
+# create a view for the chat room of the app 
+# - load chat_room.html into the function
+def chat_room(request, room_name):
+    """A view for the chat room of the ChatApp.
+
+    :param request: The HTTP request object containing information about the client's request.
+    :type request: HttpRequest
+    :param room_name: The user inputs the name of the chat room they want to enter.
+    :type room_name: str
+    :return: Return the chat_room template
+    :rtype: HttpResponse
+    """
+    # the response is the dictionary for the room names
+    # pass the context variables into a single dictionary to render in the template correctly
+    context = {"room_name": room_name}
+
+    # render the context to the chat_room template of ChatApp
+    return render(request, "chat_room.html", context)
