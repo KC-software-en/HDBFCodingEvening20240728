51789df103f41fa97cf090f5bb8e9a084adde627
KC_software_en <kisha.cairncross@gmail.com>
2024-07-22 22:59:47 +0200

    created a routing configuration in routing.py for the ChatApp to have a route to the consumer

---
+++ ChatApp/routing.py
@@ -0,0 +1,13 @@
+# https://channels.readthedocs.io/en/latest/tutorial/part_2.html#write-your-first-consumer
+from django.urls import re_path
+
+from . import consumers
+
+# call the as_asgi() classmethod to get an ASGI app that will instantiate an instance of the consumer for each user-connection
+# - similar to Django’s as_view(), which plays the same role for per-request Django view instances
+# Note: use re_path() due to limitations in URLRouter https://channels.readthedocs.io/en/latest/topics/routing.html#urlrouter
+# verify that the consumer for the /ws/chat/ROOM_NAME/ path works by running migrations to apply database changes
+# - (Django’s session framework needs the database). then start the Channels development server
+websocket_urlpatterns = [
+    re_path(r"ws/ChatApp/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    ]
